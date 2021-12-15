7860666bc30307c7b61b9c84f6e509a831a72b47
Niusha <ext.n.yousefi@mci.ir>
2021-12-15 19:04:13 +0300

    migrations

---
+++ database/migrations/2021_12_15_155957_create_replies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
